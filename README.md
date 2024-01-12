8e3695dde04b21a7d6a5a38d74244868aef0a2e8
Forrest Zhang <forrestzhang107@gmail.com>
2024-01-11 17:23:24 -0800

    update readme; ui/ux improvements

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# interview
+# Getting Started
+
+## Server Installation
+
+Rename ".env.example" to ".env"
+
+`npm install`
+`npx prisma migrate dev`
+`npm start`
+
+## Client Installation
+
+Rename ".env.example" to ".env"
+
+`npm install`
+`npm start`
--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -8,23 +8,21 @@ function PostList() {
   console.log("posts", posts);
 
   useEffect(() => {
-    listPosts()
-      .then((data) => {
-        setPosts(data);
-      })
-      .catch((err) => console.error(err));
+    listPosts().then(setPosts).catch(console.error);
   }, []);
 
   return (
     <div className="p-4">
-      <h1 className="mb-4 text-2xl font-bold">Posts</h1>
-      <Link
-        to="/create"
-        className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
-      >
-        Create Post
-      </Link>
-      <div className="mt-4">
+      <div className="mb-5 flex justify-between">
+        <h1 className="text-2xl font-bold">Posts</h1>
+        <Link
+          to="/create"
+          className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+        >
+          Create Post
+        </Link>
+      </div>
+      <div>
         {posts.map((post) => (
           <div
             key={post.id}
@@ -33,7 +31,7 @@ function PostList() {
             <Link to={`/post/${post.id}`} className="text-lg font-semibold">
               {post.title}
             </Link>
-            <p>{post.content.substring(0, 100)}...</p>
+            <p>{post.content}</p>
           </div>
         ))}
       </div>
